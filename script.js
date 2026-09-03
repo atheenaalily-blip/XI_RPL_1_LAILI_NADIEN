0312a458613b201671d2241856c543e69fa7af9a
Laili Nadien <atheenaalily@gmail.com>
2026-09-03 14:23:56 +0700

    update project

---
+++ script.js
@@ -0,0 +1,10 @@
+function tampilkanNama(){
+    document.getElementById("namaAnggota").innerHTML=`
+    <ol style="list-style-type: decimal; padding-left:5%;">
+        <li>Laili (atheenaalily@gmail.com)</li>
+        <li>Nadien (nadienqurrotulaeni@gmail.com)</li>
+    </ol>
+
+   <button onclick="loaction.reload()">Tutup Kembali</button>
+   `;
+}
